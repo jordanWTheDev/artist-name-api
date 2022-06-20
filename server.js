50bb370d31d9dfec7b0c4cfbe09163e24b680b78
jordanWTheDev <jordancodywerner@gmail.com>
2022-06-20 14:12:38 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+const rappers = {
+  "21 savage": {
+    age: 29,
+    birthName: "Holy shit thats a long name",
+    birthLocation: "London, England",
+  },
+  "chance the rapper": {
+    age: 29,
+    birthName: "Chancelor Bennet",
+    birthLocation: "Chicago, Illinois",
+  },
+  dylan: {
+    age: 29,
+    birthName: "Dylan",
+    birthLocation: "Dylan",
+  },
+};
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/:rapperName", (req, res) => {
+  const rapper = req.params.rapperName.toLowerCase();
+  if (rappers[rapper]) {
+    res.json(rappers[rapper]);
+  } else {
+    res.json(rappers["dylan"]);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}...`);
+});
